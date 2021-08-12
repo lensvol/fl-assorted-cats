0e29a79418ab42aaa64b609b945b6261738fc3d2
Kirill Borisov <lensvol@gmail.com>
2021-08-12 11:32:10 +0200

    Rewrite from scratch using XHR intercept and test slot manipulation.

--- content.js
+++ content.js
@@ -1,49 +1,12 @@
-(function() {
-    function createNewSection(name) {
-        let sectionItem = document.createElement("li");
-        sectionItem.className = "equipment-group-list__item";
-
-        let sectionDiv = document.createElement("div");
-        sectionDiv.className = "equipment-group";
-
-        let sectionHeader = document.createElement("h2");
-        sectionHeader.className = "heading heading--2 equipment-group__name";
-        sectionHeader.textContent = name;
-
-        let currentItemContainer = document.createElement("div");
-        currentItemContainer.className = "equipment-group__slot-and-available-items";
-
-        let placeholderContainer = document.createElement("div");
-        // placeholderContainer.className = "equipment-group__equipment-slot-container equipment-group__equipment-slot-container--empty u-visually-hidden"
-
-        let placeholder = document.createElement("div");
-        placeholder.className = "equipment__empty-slot"
-        placeholder.style.cssText = "width: 40px; height: 40px;";
-
-        let listOfItems = document.createElement("ul");
-        listOfItems.className = "available-item-list";
-
-        placeholderContainer.appendChild(placeholder);
-        currentItemContainer.appendChild(placeholderContainer);
-        currentItemContainer.appendChild(listOfItems);
-
-        sectionDiv.appendChild(sectionHeader);
-        sectionDiv.appendChild(currentItemContainer);
-
-        sectionItem.appendChild(sectionDiv);
-
-        return [sectionItem, listOfItems];
-    }
-
+let actualCode = '(' + function () {
     let catLabels = [
         "Sebastian the Nocturnal Smotherer",
         "Calliope the Yowler",
         "Love-Sickened Seneschal",
-        // TODO: Figure out how to move around nearby items
-        // "The Minister of War",
-        // "The Minister of Enigmas",
-        // "The Minister of Culture",
-        // "The Minister of State Affairs",
+        "The Minister of War",
+        "The Minister of Enigmas",
+        "The Minister of Culture",
+        "The Minister of State Affairs",
         "Starveling Cat",
         "Pink-Painted Cat",
         "Horatio, Finest of His Lineage",
@@ -52,65 +15,113 @@
         "Corresponding Ocelot",
         "Benvolio the Bacon Thief",
         "Freya, Scourge of Fragile Ornaments",
-        "Midnight Matriarch"
-    ]
+        "Midnight Matriarch",
+    ];
 
-    let result = createNewSection("Cats, Assorted");
-    let catSection = result[0];
-    let listOfCatItems = result[1];
+    let labelSet = {};
+    for (const label of catLabels) {
+        labelSet[label] = true;
+    }
 
-    var observer = new MutationObserver(function(mutations) {
-        observer.disconnect()
-        mutations.forEach(function(mutation) {
-            let catsItemsToAdd = [];
-            for (var i = 0; i < mutation.addedNodes.length; i++) {
-                let node = mutation.addedNodes[i];
+    let categorySet = {
+        "Companion": true,
+        "Weapon": true,
+        "Gloves": true,
+        "Hat": true,
+        "Boots": true,
+        "Clothing": true,
+    }
 
+    function modifyResponse(response) {
+        if (this.readyState === 4) {
+            // TODO: Proper URL matching
+            if (response.currentTarget.responseURL.includes("api/character/myself")) {
+                let data = JSON.parse(response.target.responseText);
+
+                let equippedItems = document.getElementsByClassName("equipped-item");
+                let equippedSet = {};
+                for (const item of equippedItems) {
+                    let qualityId = Number.parseInt(item.attributes["data-quality-id"].value);
+                    equippedSet[qualityId] = true;
+                }
+
+                for (const category of data["possessions"]) {
+                    if (!(category.name in categorySet)) continue;
+
+                    for (const candidate of category["possessions"]) {
+                        if (candidate.name in labelSet && !(candidate.id in equippedSet)) {
+                            candidate.category = "TestSlot2";
+                        }
+                    }
+                }
+
+                // Need to do this in order to reset "read-only" status on responseText attribute
+                Object.defineProperty(this, "responseText", {writable: true});
+                this.responseText = JSON.stringify(data);
+            }
+        }
+    }
+
+    function openBypass(original_function) {
+        return function (method, url, async) {
+            this.addEventListener("readystatechange", modifyResponse);
+            return original_function.apply(this, arguments);
+        };
+    }
+
+    // Taken from https://stackoverflow.com/a/41566077
+    XMLHttpRequest.prototype.open = openBypass(XMLHttpRequest.prototype.open);
+
+    let testSlotObserver = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+            if (mutation.type === "childList"
+                 && mutation.removedNodes.length > 0
+                 && mutation.target.nodeName.toLowerCase() === "li") {
+                for (const child of mutation.target.parentElement.children) {
+                    if (child.children.length === 0) {
+                        child.className += " u-visually-hidden"
+                    }
+                }
+            }
+        })
+    })
+
+    let itemSectionObserver = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+            mutation.addedNodes.forEach(function (node) {
                 if (node.nodeName.toLowerCase() === "div") {
-                    let equipmentGroupList = node.getElementsByClassName("equipment-group-list");
+                    let equipmentGroups = node.getElementsByClassName("equipment-group-list__item");
+                    for (const group of equipmentGroups) {
+                        if (group.children.length === 0) {
+                            continue;
+                        }
 
-                    if (equipmentGroupList.length > 0) {
-                        let groupList = equipmentGroupList[0];
-                        let allItems = groupList.querySelectorAll("li.available-item-list__item");
-                        for (var a = 0; a < allItems.length; a++) {
-                            let label = allItems[a].firstChild.firstChild.ariaLabel;
-                            for (var b = 0; b < catLabels.length; b++) {
-                                if (label.includes(catLabels[b])) {
-                                    catsItemsToAdd.push(allItems[a]);
-                                    break;
+                        if (group.firstChild.firstChild.textContent === "TestSlot2") {
+                            let header = group.firstChild.firstChild;
+                            header.textContent = "Cats, Assorted";
+
+                            group.firstChild.children[1].firstChild.className = "";
+                            for (const child of group.firstChild.children[1].children[1].children) {
+                                if (child.children.length === 0) {
+                                    child.className += " u-visually-hidden";
                                 }
                             }
-                        }
-                        groupList.insertBefore(catSection, groupList.children[3]);
-                    }
-                } else if (node.nodeName.toLowerCase() === "li" && node.className === "available-item-list__item") {
-                    for (var b = 0; b < catLabels.length; b++) {
-                        let label = node.firstChild.firstChild.ariaLabel;
-                        if (label.includes(catLabels[b]) && node.parentElement !== listOfCatItems && !node.hasAttribute("wrappedAroundCat")) {
-                            catsItemsToAdd.push(node);
+
+                            itemSectionObserver.disconnect();
+                            testSlotObserver.observe(group.firstChild.children[1], {childList: true, subtree: true})
                             break;
                         }
                     }
                 }
-            }
-
-            if (catsItemsToAdd.length > 0) {
-                for (var i = 0; i < catsItemsToAdd.length; i++) {
-                    let parent = catsItemsToAdd[i].parentElement;
-                    let catElement = catsItemsToAdd[i];
-                    catsItemsToAdd[i].onclick = function () {
-                        catElement.setAttribute("wrappedAroundCat", true);
-                        catElement.className = "u-visually-hidden";
-                        parent.appendChild(catElement);
-                    }
-
-                    listOfCatItems.appendChild(catsItemsToAdd[i]);
-                }
-
-            }
-            observer.observe(document, {childList: true, subtree: true, attributes: true});
+            });
         });
     });
-    observer.observe(document, { childList: true, subtree: true, attributes: true });
 
-})();
+    itemSectionObserver.observe(document, {attributes: true, childList: true, subtree: true});
+} + ')();';
+
+// Taken from https://stackoverflow.com/a/9517879
+let script = document.createElement('script');
+script.textContent = actualCode;
+(document.head || document.documentElement).appendChild(script);
+script.remove();
