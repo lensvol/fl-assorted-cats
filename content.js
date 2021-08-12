2e6fbf4403d5ab941d0c6bb03a5688423d57bb0f
Kirill Borisov <lensvol@gmail.com>
2021-08-12 18:21:38 +0200

    Add a lot of comments and prettify code somewhat.

--- content.js
+++ content.js
@@ -1,29 +1,27 @@
 let actualCode = '(' + function () {
-    let catLabels = [
-        "Sebastian the Nocturnal Smotherer",
-        "Calliope the Yowler",
-        "Love-Sickened Seneschal",
-        "The Minister of War",
-        "The Minister of Enigmas",
-        "The Minister of Culture",
-        "The Minister of State Affairs",
-        "Starveling Cat",
-        "Pink-Painted Cat",
-        "Horatio, Finest of His Lineage",
-        "Rubbery Feline",
-        "August Feline",
-        "Corresponding Ocelot",
-        "Benvolio the Bacon Thief",
-        "Freya, Scourge of Fragile Ornaments",
-        "Midnight Matriarch",
-    ];
+    const VISUALLY_HIDDEN_STYLE = "u-visually-hidden";
 
-    let labelSet = {};
-    for (const label of catLabels) {
-        labelSet[label] = true;
-    }
+    // TODO: Fill it out with the remaining felines from wiki.
+    const CAT_LABEL_SET = {
+        "Sebastian the Nocturnal Smotherer": true,
+        "Calliope the Yowler": true,
+        "Love-Sickened Seneschal": true,
+        "The Minister of War": true,
+        "The Minister of Enigmas": true,
+        "The Minister of Culture": true,
+        "The Minister of State Affairs": true,
+        "Starveling Cat": true,
+        "Pink-Painted Cat": true,
+        "Horatio, Finest of His Lineage": true,
+        "Rubbery Feline": true,
+        "August Feline": true,
+        "Corresponding Ocelot": true,
+        "Benvolio the Bacon Thief": true,
+        "Freya, Scourge of Fragile Ornaments": true,
+        "Midnight Matriarch": true,
+    };
 
-    let usedTestSlot = "TestSlot3";
+    const USED_TEST_SLOT = "TestSlot3";
 
     let categorySet = {
         "Companion": true,
@@ -35,10 +33,32 @@ let actualCode = '(' + function () {
     }
 
     function modifyResponse(response) {
+        /*
+        The idea itself is pretty simple: trick FL UI to put specific items into specific slots
+        by modifying data that is returned from 'api/character/myself' endpoint.
+
+        Each item dictionary has 'category' key that corresponds to one of the category IDs
+        hardcoded into the FL UI source code, so by patching that value you can direct UI
+        to "misplace" it into the category that you need. For all intents and purposes, it will be
+        treated as a fully functioning item and if you click on it will be equipped into the proper
+        slot.
+
+        Here we make use of the "test slots" that are left in the "Possesions" tab, praying that FBG
+        will not decide to remove them in the future.
+         */
         if (this.readyState === 4) {
             // TODO: Proper URL matching
             if (response.currentTarget.responseURL.includes("api/character/myself")) {
                 let data = JSON.parse(response.target.responseText);
+
+                /*
+                 Here is the tricky part: this response does not contain any indication that if
+                 specific item is equipped or not, and if you mark equipped item as belonging
+                 to the test slot then it would not possible to un-equip it.
+
+                 To prevent that, we use a heuristic that tries to get the list of the currently
+                 equipped items so we can skip patching them.
+                 */
 
                 let equippedItems = document.getElementsByClassName("equipped-item");
                 let equippedSet = {};
@@ -48,11 +68,13 @@ let actualCode = '(' + function () {
                 }
 
                 for (const category of data["possessions"]) {
+
+                    // Only some of the categories can contain cats, so we'll just skip others to save time.
                     if (!(category.name in categorySet)) continue;
 
                     for (const candidate of category["possessions"]) {
-                        if (candidate.name in labelSet && !(candidate.id in equippedSet)) {
-                            candidate.category = usedTestSlot;
+                        if (candidate.name in CAT_LABEL_SET && !(candidate.id in equippedSet)) {
+                            candidate.category = USED_TEST_SLOT;
                         }
                     }
                 }
@@ -64,6 +86,16 @@ let actualCode = '(' + function () {
         }
     }
 
+    /*
+     HERE BE DRAGONS
+
+     I would have really liked to just use `webRequest` API and modify the responses transparently,
+     but until https://bugs.chromium.org/p/chromium/issues/detail?id=104058 is resolved that
+     would have lead to more convoluted code base due to the need for separate interception
+     mechanisms for both Chrome and Firefox (don't start me on Safari T_T).
+
+     Solution taken from https://stackoverflow.com/a/41566077
+     */
     function openBypass(original_function) {
         return function (method, url, async) {
             this.addEventListener("readystatechange", modifyResponse);
@@ -71,66 +103,78 @@ let actualCode = '(' + function () {
         };
     }
 
-    // Taken from https://stackoverflow.com/a/41566077
     XMLHttpRequest.prototype.open = openBypass(XMLHttpRequest.prototype.open);
 
-    let testSlotObserver = new MutationObserver(function (mutations) {
+    /*
+    For some reason, items equipped from the test slot leave ugly "holes behind", which
+    are actually empty <li> nodes.
+
+    As a matter of convenience, we listen to the changes on those elements and hide them
+    if their contents are removed.
+
+    Conversely, if their contents are restored due to item being unequipped, we will show them again.
+    */
+    let itemContentsObserver = new MutationObserver(function (mutations) {
         mutations.forEach(function (mutation) {
             if (mutation.type === "childList"
-                 && mutation.removedNodes.length > 0
                  && mutation.target.nodeName.toLowerCase() === "li") {
 
-                var emptyChildren = [];
-                for (const child of mutation.target.parentElement.children) {
-                    if (!child.hasChildNodes()) {
-                        emptyChildren.push(child);
-                    }
-                }
+                let alreadyHidden = mutation.target.classList.contains(VISUALLY_HIDDEN_STYLE);
 
-                let itemList = mutation.target.parentElement;
-                emptyChildren.forEach(child => {
-                    child.remove();
-                    itemList.appendChild(child);
-                })
+                if (mutation.removedNodes.length > 0) {
+                    mutation.target.classList.add(VISUALLY_HIDDEN_STYLE)
+                } else if (mutation.addedNodes.length > 0){
+                    mutation.target.classList.remove(VISUALLY_HIDDEN_STYLE)
+                }
             }
         })
     })
 
-    let itemSectionObserver = new MutationObserver(function (mutations) {
+    /*
+    To my understanding, on the initial load FL UI will render most of its content at once and
+    so mutation observer will not trigger on filled equipment list. That is because technically
+    it is not being added as it is, but as a part of a larger 'div'.
+     */
+    let testSlotObserver = new MutationObserver(function (mutations) {
         mutations.forEach(function (mutation) {
             mutation.addedNodes.forEach(function (node) {
+                // TODO: Target those layers more precisely
                 if (node.nodeName.toLowerCase() === "div") {
                     let equipmentGroups = node.getElementsByClassName("equipment-group-list__item");
                     for (const group of equipmentGroups) {
+                        // Skip groups without any items in them
                         if (group.children.length === 0) {
                             continue;
                         }
 
-                        let headers = group.getElementsByTagName("h2");
-                        if (headers.length === 0) {
-                            continue;
-                        }
+                        let header = group.getElementsByTagName("h2")[0];
 
-                        if (headers[0].textContent === usedTestSlot) {
-                            headers[0].textContent = "Cats, Assorted";
+                        if (header.textContent === USED_TEST_SLOT) {
+                            header.textContent = "Cats, Assorted";
 
                             let itemList = group.getElementsByTagName("ul")[0];
+                            /*
+                            Since nothing can be equipped in the test slot, we'll just hide the container
+                            for the currently equipped item.
+
+                            TODO: Make it Horatio's seat of honor?
+                            */
+
                             let equippedContainer = group.getElementsByClassName("equipment-group__equipment-slot-container")[0];
                             equippedContainer.className = "";
 
-                            var emptyChildren = [];
+                            // As items in that group are already pre-filled, our observer will not trigger yet.
                             for (const child of itemList.children) {
                                 if (!child.hasChildNodes()) {
-                                    emptyChildren.push(child);
+                                    child.classList.add(VISUALLY_HIDDEN_STYLE)
                                 }
                             }
-                            emptyChildren.forEach(child => {
-                                child.remove();
-                                itemList.appendChild(child)
-                            })
+                            itemContentsObserver.observe(itemList, {childList: true, subtree: true})
 
-                            itemSectionObserver.disconnect();
-                            testSlotObserver.observe(itemList, {childList: true, subtree: true})
+
+                            // Since we already found the test slot and processed it, it is no longer
+                            // necessary to monitor changes on the entire document.
+                            testSlotObserver.disconnect();
                             break;
                         }
                     }
@@ -139,9 +183,10 @@ let actualCode = '(' + function () {
         });
     });
 
-    itemSectionObserver.observe(document, {attributes: true, childList: true, subtree: true});
+    testSlotObserver.observe(document, {childList: true, subtree: true});
 } + ')();';
 
+// Here we inject our code above into the context of the page itself, since we need to patch its XHR mechanisms.
 // Taken from https://stackoverflow.com/a/9517879
 let script = document.createElement('script');
 script.textContent = actualCode;
