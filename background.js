5f009dfd34320cea93fea10155304070712614ab
Kirill Borisov <lensvol@gmail.com>
2021-10-07 22:20:30 +0200

    Ensure that on first install we save correct preset to DB.

--- background.js
+++ background.js
@@ -13,38 +13,8 @@ chrome.runtime.onInstalled.addListener(function (details) {
     if (saveInitialSettings) {
         chrome.storage.local.set({
             settings: {
-                slotName: 'Cats, Assorted',
-                items: [
-                    'Sebastian the Nocturnal Smotherer',
-                    'Calliope the Yowler',
-                    'Love-Sickened Seneschal',
-                    'The Minister of War',
-                    'The Minister of Enigmas',
-                    'The Minister of Culture',
-                    'The Minister of State Affairs',
-                    'Starveling Cat',
-                    'Pink-Painted Cat',
-                    'Horatio, Finest of His Lineage',
-                    'Grubby Kitten',
-                    'Gustav the Ankle Weaver',
-                    'Rubbery Feline',
-                    'August Feline',
-                    'Corresponding Ocelot',
-                    'Benvolio the Bacon Thief',
-                    'Freya, Scourge of Fragile Ornaments',
-                    'Midnight Matriarch',
-                    'Midnight Matriarch of the Menagerie of Roses',
-                    'Bengal Tigress',
-                    'Extravagantly-Titled Tigress',
-                    'Tomb-Lion',
-                    'Parabolan Panther',
-                    'Parabolan Kitten',
-                    'Wretched Mog',
-                    'Princeling of the Wakeful Court',
-                    'Morally and Physically Flexible Rubbery Cat',
-                    'A Short-Tempered Shorthair',
-                    'Lyon Pursuivant of Arms Extraordinary'
-                ]
+                slotName: DEFAULT_PRESET_KEY,
+                items: SLOT_CONTENTS_PRESETS.get(DEFAULT_PRESET_KEY),
             }
         }, () => { console.log('[FL Assorted Cats] Default settings saved into DB') });
     }
